b119d6f225dab1c737628f54ecf6f9b66486cb1f
许海辉 <xuhaihui@newshiying.onaliyun.com>
2024-10-08 18:25:18 +0800

    feat:add icon component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { IconAdd,IconEmail } from './Icon/createIcon';
+
+function App() {
+  return (
+    <div style={ {padding: '50px'} }>
+      <IconAdd></IconAdd>
+      <IconEmail></IconEmail>
+    </div>
+  );
+}
+
+export default App;
